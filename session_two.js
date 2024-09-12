77d161dfadbf3e12a90871ef316287e144d606f3
TRACY ADONGO PAULA <tracy8paula@gmail.com>
2024-09-12 21:41:48 +0300

    week-2-session-2 file
    this is basic syntax to JavaScript and focus on variables.

---
+++ session_two.js
@@ -0,0 +1,32 @@
+// my first attempt to javascript application
+console.log("Hello world");
+
+// student info
+console.log("Adongo Tracy Paula");
+console.log("b27291");
+console.log("Data Science and Analytics");
+
+//VARIABLES
+//Variable Initialization > define the name of the variable and give it a value in a line
+ var my_name = "Adongo Tracy Paula";
+console.log(my_name);
+
+//Variable declaration > name the variable
+var height;
+
+//Variable assignment
+height = 156;
+console.log(height);
+
+// Let > opt for this as a newer version instead of var!!!
+let bestie = "Treasure";
+let area;
+area = 45.3;
+
+// Const >> must always intialise your variables here as they MUST be constant
+const pie = 3.14;
+const k =2;
+
+// concatenating in js
+console.log(height, area);
+console.log(my_name, area);
