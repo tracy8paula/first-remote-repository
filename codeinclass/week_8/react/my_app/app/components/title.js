3b455eb26de3baaf3bd60ea6f64b6dea01f329bc
TRACY ADONGO PAULA <tracy8paula@gmail.com>
2024-11-01 11:12:53 +0300

    the website push
    This is basically adding on to our app in modifications

---
+++ codeinclass/week_8/react/my_app/app/components/title.js
@@ -0,0 +1,8 @@
+function title(){
+    return(
+        <div>
+            <h1> My Heading</h1>
+        </div>
+    )
+};
+export default title;
--- codeinclass/week_8/react/my_app/app/note.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Note({ text }) {
-  return <div>{text}</div>;
-}
-
-export default Note;
